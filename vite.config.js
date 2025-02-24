6455c3a321289e3f3098782c452e281cd3db4211
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-24 12:34:24 +0300

    adds transaction id to admin agent

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3501
+    port: 3509
   },
   plugins: [vue()],
   resolve: {
